02ad4023e0c2c17d4bb959ad2fcae7ee6b982677
John Nielsen <johnhayesnielsen@gmail.com>
2020-08-25 11:28:31 -0600

    added comments routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
-//add prefix of `/pizzas` to routs created un `pizza-routes.js`
+//add prefix of `/pizzas` to routs created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
